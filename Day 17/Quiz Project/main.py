5aaea92803d5fbb7bf1bff8ba9eed959cdc2a398
ayushsharma-git <ayushsharmaiima@gmail.com>
2023-12-28 15:38:38 +0100

    Quiz project

---
+++ Day 17/Quiz Project/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_obj = Question(question["text"], question["answer"])
+    question_bank.append(question_obj)
+
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
+print(f"Your final score was: {quiz_brain.score}/{quiz_brain.question_number}")
+
--- Day 17/Start/main.py
+++ Day 17/Start/main.py
@@ -1,5 +1,16 @@
 #PascalCase class
+'''
+has attributes
+does methods
+'''
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
-user_1 = User()
+
+user_1 = User(9, "ayush")
+
+print(user_1.username)
