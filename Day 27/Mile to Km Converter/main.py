49b817348573c8d0601be2a990e263822be97c60
ayushsharma-git <ayushsharmaiima@gmail.com>
2024-01-10 22:38:19 +0100

    Final commit for day 27

--- Day 27/Mile to Km Converter/main.py
+++ Day 27/Mile to Km Converter/main.py
@@ -2,20 +2,27 @@ from tkinter import *
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(500, 300)
+window.minsize(50, 30)
 window.config(padx=20, pady=20)
 
-mile_entry = Entry()
+
+def calculate():
+    in_km = round(float(mile_entry.get()) * 1.609, ndigits=2)
+    in_km_label.config(text=in_km)
+
+
+mile_entry = Entry(width=3)
 is_equal_to_label = Label(text="is equal to")
 mile_label = Label(text="Miles")
 km_label = Label(text="Km")
-button = Button(text="Calculate")
+in_km_label = Label(text=0)
+button = Button(text="Calculate", command=calculate)
 
-mile_entry.grid(column=1, row= 0)
-is_equal_to_label.grid(column=0, row= 2)
-mile_label.grid(column=2, row= 0)
-mile_label.grid(column=2, row= 0)
+mile_entry.grid(column=1, row=0)
+is_equal_to_label.grid(column=0, row=1)
+mile_label.grid(column=2, row=0)
+km_label.grid(column=2, row=1)
+in_km_label.grid(column=1, row=1)
+button.grid(column=1, row=2)
 
-
-
-
+window.mainloop()
